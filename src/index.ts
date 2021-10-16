36790a30f3311d907aa4cbbd456e77f664752d12
Skyzi000 <skyzi000r@gmail.com>
2021-10-16 19:02:10 +0900

    ownerの設定を不要にし、通知チャンネルが設定されていないときはVCに入った人にDMを飛ばすようにした

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Channel, Client, Intents, TextBasedChannels, User, VoiceState } from "discord.js";
+import { Client, Intents, TextBasedChannels, VoiceState } from "discord.js";
 
 // .envファイルから環境変数の読み込み
 if (process.env.NODE_ENV !== "production") {
@@ -13,15 +13,10 @@ const client: Client = new Client({
         Intents.FLAGS.DIRECT_MESSAGES,
     ]
 });
-if (process.env.DISCORD_BOT_OWNER_ID === undefined)
-    throw new Error("DISCORD_BOT_OWNER_ID is undefined");
 
-const ownerId = process.env.DISCORD_BOT_OWNER_ID;
-let owner: User;
 let notifyChannel: TextBasedChannels | undefined;
 
 client.once("ready", async () => {
-    owner = await client.users.fetch(ownerId);
     console.log(`${client.user?.username} is ready!`);
 });
 
@@ -34,7 +29,7 @@ client.on("message", async message => {
     if (client.user !== null && message.mentions.users.has(client.user.id)) {
         const cmds = message.content.split(" ").slice(1);
         console.log(`Commands: ${cmds.join(", ")}`);
-        switch (cmds[0]) {
+        switch (cmds[0].trim()) {
             case "setnc":
             case "set_notification_channel":
             case "通知チャンネル":
@@ -56,11 +51,11 @@ client.login(process.env.DISCORD_BOT_TOKEN);
 async function onVoiceStateUpdate(oldState: VoiceState, newState: VoiceState) {
     if (oldState.channelId !== newState.channelId) {
         if (newState.channelId !== null && newState.channel?.members.size === 1) {
-            const username = newState.member?.displayName;
             if (notifyChannel === undefined) {
-                owner.send("通知するチャンネルを`setnc`コマンドで設定してください。");
+                newState.member?.send("通話開始を通知するチャンネルが設定されていません。\n`setnc`コマンドを使って設定してください。");
                 return;
             }
+            const username = newState.member?.displayName;
             await notifyChannel.send(`${username} さんが <#${newState.channelId}> で通話を開始しました。`);
         }
     }
