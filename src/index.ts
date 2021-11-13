17000dc23ac06de7d7e2d44517bdb1cf01989b25
Skyzi000 <skyzi000r@gmail.com>
2021-11-13 21:30:33 +0900

    sqlite上のkeyvデータベースを利用して、通知チャンネルを保持できるようにした

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import { Client, Intents, TextBasedChannels, VoiceState } from "discord.js";
+import Keyv from 'keyv';
+
+const notifyChannels = new Keyv("sqlite://db.sqlite", { table: "notifyChannel" });
 
 // .envファイルから環境変数の読み込み
 if (process.env.NODE_ENV !== "production") {
@@ -14,19 +17,21 @@ const client: Client = new Client({
     ]
 });
 
-let notifyChannel: TextBasedChannels | undefined;
-
 client.once("ready", async () => {
-    console.log(`${client.user?.username} is ready!`);
+    if (client.user == null) {
+        console.log("client.user is null!");
+        return;
+    }
+    console.log(`${client.user.username} is ready!`);
 });
 
 client.on("voiceStateUpdate", async (os, ns) => onVoiceStateUpdate(os, ns));
 
 client.on("message", async message => {
-    if (message.author.bot) {
+    if (message.author.bot || message.guildId == null || client.user == null) {
         return;
     }
-    if (client.user !== null && message.mentions.users.has(client.user.id)) {
+    if (message.mentions.users.has(client.user.id)) {
         const cmds = message.content.split(" ").slice(1).filter((value) => value.trim() !== "");
         console.log(`Commands: ${cmds.join(", ")}`);
         switch (cmds[0].trim()) {
@@ -34,13 +39,13 @@ client.on("message", async message => {
             case "set_notification_channel":
             case "通知チャンネル":
                 if (cmds.length == 1) {
-                    notifyChannel = message.channel;
-                    await message.reply(`<#${notifyChannel.id}> を通知チャンネルに設定しました。`);
+                    setNotifyChannel(message.channel, message.guildId);
+                    await message.reply(`<#${message.channelId}> を通知チャンネルに設定しました。`);
                 }
                 else {
                     const nc = await client.channels.fetch(cmds[1].trim());
                     if (nc !== null && nc.isText()) {
-                        notifyChannel = nc;
+                        setNotifyChannel(nc, message.guildId);
                         await message.reply(`<#${nc.id}> を通知チャンネルに設定しました。`);
                     }
                     else {
@@ -59,6 +64,7 @@ client.on("message", async message => {
 async function onVoiceStateUpdate(oldState: VoiceState, newState: VoiceState) {
     if (oldState.channelId !== newState.channelId) {
         if (newState.channelId !== null && newState.channel?.members.size === 1) {
+            const notifyChannel = await getNotifyChannel(newState.guild.id);
             if (notifyChannel === undefined) {
                 newState.member?.send("通話開始を通知するチャンネルが設定されていません。\n`setnc`コマンドを使って設定してください。");
                 return;
@@ -67,6 +73,22 @@ async function onVoiceStateUpdate(oldState: VoiceState, newState: VoiceState) {
             await notifyChannel.send(`${username} さんが <#${newState.channelId}> で通話を開始しました。`);
         }
     }
+}
+
+async function setNotifyChannel(channel: TextBasedChannels, guildId: string) {
+    await notifyChannels.set(guildId, channel.id);
+}
+
+async function getNotifyChannel(guildId: string): Promise<TextBasedChannels | undefined> {
+    const id = await notifyChannels.get(guildId);
+    if (!id) {
+        return undefined;
+    }
+    const ch = await client.channels.fetch(guildId);
+    if (ch?.isText()) {
+        return ch;
+    }
+    return undefined;
 }
 
 client.login(process.env.DISCORD_BOT_TOKEN);
